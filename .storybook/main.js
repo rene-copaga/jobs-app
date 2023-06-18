0c12e94ce449b3d6bfc70ee1b52e55b45dfaf70c
Rene Copaga <rene.copaga@gmail.com>
2023-06-18 11:20:41 -0400

    -Storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,31 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpackplugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  features: {
+    emotionAlias: false,
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(
+          __dirname,
+          '../tsconfig.json'
+        ),
+      })
+    );
+    return config;
+  },
+};
