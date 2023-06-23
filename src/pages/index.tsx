f121d73ccd5705602a374a158344f344391db665
Rene Copaga <rene.copaga@gmail.com>
2023-06-23 19:33:16 -0400

    Next.js routing Next.js rendering strategies -Client-side rendering -Server-side rendering Next.js SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+import { Center } from '@chakra-ui/react';
+
 import { Button } from '@/components/button';
 import { InputField } from '@/components/form';
 import { Link } from '@/components/link';
+import { Seo } from '@/components/seo/seo';
 const LandingPage = () => {
   return (
     <>
-      <Button variant="solid" type="button">
-        Click Me
-      </Button>
-      <br />
-      <InputField label="Name" />
-      <br />
-      <Link href="/">Home</Link>
+      <Seo title="Jobs App" />
+      <Center>
+        <Button variant="solid" type="button">
+          Click Me
+        </Button>
+        <br />
+        <InputField label="Name" />
+        <br />
+        <Link href="/">Home</Link>
+      </Center>
     </>
   );
 };
